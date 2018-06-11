23299146fe52c3fccec8eb8b3be5c9a358a885ba
Chris <Christian.Feier@gmail.com>
2018-06-12 00:03:08 +0200

    - made exception not null

--- JKMP3GainWrapper/src/main/java/com/cf/jkmp3gainwrapper/exception/MP3GainException.kt
+++ JKMP3GainWrapper/src/main/java/com/cf/jkmp3gainwrapper/exception/MP3GainException.kt
@@ -2,7 +2,7 @@ package com.cf.jkmp3gainwrapper.exception
 
 class MP3GainException : java.lang.Exception {
 
-    constructor(message: String, ex: Exception?) : super(message, ex)
+    constructor(message: String, ex: Exception) : super(message, ex)
     constructor(message: String) : super(message)
     constructor(ex: Exception) : super(ex)
 }
